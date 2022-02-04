5301454fcfff60e2753b23443b2235afb247e573
Bryan <bryan.flores04@epn.edu.ec>
2022-02-04 14:17:26 -0500

    Laboratorio 8 Firestore

--- app/src/main/java/com/floresbryan/cazarpatos/RankingActivity.kt
+++ app/src/main/java/com/floresbryan/cazarpatos/RankingActivity.kt
@@ -2,46 +2,80 @@ package com.floresbryan.cazarpatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.floresbryan.cazarpatos.database.RankingPlayerDBHelper
+import com.google.firebase.firestore.Query
+
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 
 class RankingActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ranking)
 
-        OperacionesSqLite()
-        GrabarRankingSQLite()
-        LeerRankingsSQLite()
+//        OperacionesSqLite()
+//        GrabarRankingSQLite()
+//        LeerRankingsSQLite()
+        consultarPuntajeJugadores()
     }
-    fun OperacionesSqLite(){
-        RankingPlayerDBHelper(this).deleteAllRanking()
-        RankingPlayerDBHelper(this).insertRankingByQuery(Jugador("Jugador9",10))
-        val patosCazados = RankingPlayerDBHelper(this).readDucksHuntedByPlayer("Jugador9")
-        RankingPlayerDBHelper(this).updateRanking(Jugador("Jugador9",5))
-        RankingPlayerDBHelper(this).deleteRanking("Jugador9")
-        RankingPlayerDBHelper(this).insertRanking(Jugador("Jugador9",7))
-        val players = RankingPlayerDBHelper(this).readAllRankingByQuery()
+    fun consultarPuntajeJugadores() {
+        val db = Firebase.firestore
+        db.collection("ranking")
+            .orderBy("patosCazados", Query.Direction.DESCENDING)
+            .get()
+            .addOnSuccessListener { result ->
+                Log.d(EXTRA_LOGIN, "Success getting documents")
+                var jugadores = ArrayList<Jugador>()
+                for (document in result) {
+                    //val jugador = document.toObject(Jugador::class.java)
+                    val jugador = document.toObject<Jugador>()
+                    jugadores.add(jugador)
+                }
+                //Poblar en RecyclerView información usando mi adaptador
+                val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking);
+                recyclerViewRanking.layoutManager = LinearLayoutManager(this);
+                recyclerViewRanking.adapter = RankingAdapter(jugadores);
+                recyclerViewRanking.setHasFixedSize(true);
+            }
+            .addOnFailureListener { exception ->
+                Log.w(EXTRA_LOGIN, "Error getting documents.", exception)
+                Toast.makeText(this, "Error al obtener datos de jugadores", Toast.LENGTH_LONG)
+                    .show()
+            }
     }
-    fun GrabarRankingSQLite(){
-        val jugadores = arrayListOf(
-            Jugador("Jugador1", 10),
-            Jugador("Jugador2", 6),
-            Jugador("Jugador3", 3),
-            Jugador("Jugador4", 9)
-        )
-        jugadores.sortByDescending { it.patosCazados }
-        for(jugador in jugadores){
-            RankingPlayerDBHelper(this).insertRanking(jugador)
-        }
-    }
-    fun LeerRankingsSQLite(){
-        val jugadoresSQLite = RankingPlayerDBHelper(this).readAllRanking()
-        val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking)
-        recyclerViewRanking.layoutManager = LinearLayoutManager(this)
-        recyclerViewRanking.adapter = RankingAdapter(jugadoresSQLite)
-        recyclerViewRanking.setHasFixedSize(true)
-    }
+
+//    fun OperacionesSqLite(){
+//        RankingPlayerDBHelper(this).deleteAllRanking()
+//        RankingPlayerDBHelper(this).insertRankingByQuery(Jugador("Jugador9",10))
+//        val patosCazados = RankingPlayerDBHelper(this).readDucksHuntedByPlayer("Jugador9")
+//        RankingPlayerDBHelper(this).updateRanking(Jugador("Jugador9",5))
+//        RankingPlayerDBHelper(this).deleteRanking("Jugador9")
+//        RankingPlayerDBHelper(this).insertRanking(Jugador("Jugador9",7))
+//        val players = RankingPlayerDBHelper(this).readAllRankingByQuery()
+//    }
+//    fun GrabarRankingSQLite(){
+//        val jugadores = arrayListOf(
+//            Jugador("Jugador1", 10),
+//            Jugador("Jugador2", 6),
+//            Jugador("Jugador3", 3),
+//            Jugador("Jugador4", 9)
+//        )
+//        jugadores.sortByDescending { it.patosCazados }
+//        for(jugador in jugadores){
+//            RankingPlayerDBHelper(this).insertRanking(jugador)
+//        }
+//    }
+//    fun LeerRankingsSQLite(){
+//        val jugadoresSQLite = RankingPlayerDBHelper(this).readAllRanking()
+//        val recyclerViewRanking: RecyclerView = findViewById(R.id.recyclerViewRanking)
+//        recyclerViewRanking.layoutManager = LinearLayoutManager(this)
+//        recyclerViewRanking.adapter = RankingAdapter(jugadoresSQLite)
+//        recyclerViewRanking.setHasFixedSize(true)
+//    }
 }
 
